6594bcbbec41efc2930b4adf7051b1dc9cfa4d70
Ztj <ztj1993@gmail.com>
2019-09-03 15:05:53 +0800

    Update entrypoint.sh

--- python-package-build/entrypoint.sh
+++ python-package-build/entrypoint.sh
@@ -5,21 +5,22 @@ set -e
 package_name=$(python setup.py --name)
 echo "package name: ${package_name}"
 
-TEST_PYPI_ENABLE=${TEST_PYPI_ENABLE:-"false"}
-TEST_PYPI_USERNAME=${TEST_PYPI_USERNAME:-"not_set"}
-TEST_PYPI_PASSWORD=${TEST_PYPI_PASSWORD:-"not_set"}
+TEST_ENABLE=${TEST_ENABLE:-"false"}
+TEST_URL=${TEST_URL:-"https://test.pypi.org/legacy/"}
+TEST_USERNAME=${TEST_USERNAME:-"not_set"}
+TEST_PASSWORD=${TEST_PASSWORD:-"not_set"}
 
-PYPI_ENABLE=${TEST_PYPI_ENABLE:-"false"}
+PYPI_ENABLE=${TEST_ENABLE:-"false"}
 PYPI_USERNAME=${PYPI_USERNAME:-"not_set"}
 PYPI_PASSWORD=${PYPI_PASSWORD:-"not_set"}
 
 python setup.py sdist bdist_wheel
 
-if [[ "${TEST_PYPI_ENABLE}" == "true" ]]; then
+if [[ "${TEST_ENABLE}" == "true" ]]; then
     python -m twine upload \
-      --repository-url https://test.pypi.org/legacy/ \
-      --username ${TEST_PYPI_USERNAME} \
-      --password ${TEST_PYPI_PASSWORD} \
+      --repository-url ${TEST_URL} \
+      --username ${TEST_USERNAME} \
+      --password ${TEST_PASSWORD} \
       dist/*
 
     python -m pip install --index-url https://test.pypi.org/simple/ --no-deps ${package_name}
