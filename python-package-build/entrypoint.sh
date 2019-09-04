0836a41fd60425b1bd3f64b26841c70f00f838b8
Ztj <ztj1993@gmail.com>
2019-09-04 11:46:55 +0800

    Update entrypoint.sh

--- python-package-build/entrypoint.sh
+++ python-package-build/entrypoint.sh
@@ -2,29 +2,31 @@
 
 set -e
 
-package_name=$(python setup.py --name)
-echo "package name: ${package_name}"
+export PACKAGE_NAME=$(python setup.py --name)
+export PACKAGE_VERSION=$(python setup.py --version)
+echo "package: ${PACKAGE_NAME}==${PACKAGE_VERSION}"
 
-TEST_ENABLE=${TEST_ENABLE:-"false"}
-TEST_URL=${TEST_URL:-"https://test.pypi.org/legacy/"}
-TEST_USERNAME=${TEST_USERNAME:-"not_set"}
-TEST_PASSWORD=${TEST_PASSWORD:-"not_set"}
+export TEST_ENABLE=${TEST_ENABLE:-"false"}
+export TEST_UPLOAD_URL=${TEST_UPLOAD_URL:-"https://test.pypi.org/legacy/"}
+export TEST_INDEX_URL=${TEST_INDEX_URL:-"https://test.pypi.org/simple/"}
+export TEST_USERNAME=${TEST_USERNAME:-"not_set"}
+export TEST_PASSWORD=${TEST_PASSWORD:-"not_set"}
 
-PYPI_ENABLE=${TEST_ENABLE:-"false"}
-PYPI_USERNAME=${PYPI_USERNAME:-"not_set"}
-PYPI_PASSWORD=${PYPI_PASSWORD:-"not_set"}
+export PYPI_ENABLE=${TEST_ENABLE:-"false"}
+export PYPI_USERNAME=${PYPI_USERNAME:-"not_set"}
+export PYPI_PASSWORD=${PYPI_PASSWORD:-"not_set"}
 
-python setup.py sdist bdist_wheel
+python setup.py bdist_wheel --build-number $(date +%Y%m%d%H%M%S)
 
 if [[ "${TEST_ENABLE}" == "true" ]]; then
     python -m twine upload \
-      --repository-url ${TEST_URL} \
+      --repository-url ${TEST_UPLOAD_URL} \
       --username ${TEST_USERNAME} \
       --password ${TEST_PASSWORD} \
       dist/*
 
-    python -m pip install --index-url https://test.pypi.org/simple/ --no-deps ${package_name}
-    python -m pip uninstall ${package_name}
+    python -m pip install --index-url ${TEST_INDEX_URL} --no-deps ${PACKAGE_NAME}==${PACKAGE_VERSION}
+    python -m pip uninstall ${PACKAGE_NAME}
 fi
 
 if [[ "${PYPI_ENABLE}" == "true" ]]; then
@@ -33,8 +35,8 @@ if [[ "${PYPI_ENABLE}" == "true" ]]; then
       --password ${PYPI_PASSWORD} \
       dist/*
 
-    python -m pip install ${package_name}
-    python -m pip uninstall ${package_name}
+    python -m pip install ${PACKAGE_NAME}
+    python -m pip uninstall ${PACKAGE_NAME}
 fi
 
 exec "$@"
